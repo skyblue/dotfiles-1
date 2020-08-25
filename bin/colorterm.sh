a43a4368c4e5f1ac35c1570273390970ab956090
2O4 <35725720+2O4@users.noreply.github.com>
2020-08-25 16:52:23 +0200

    tmux screen background changed based on usage (active or not, or ssh)

---
+++ bin/colorterm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# author: Bryan Gilbert
+# source: https://bryangilbert.com/post/etc/term/dynamic-ssh-terminal-background-colors/
+#
+
+if [[ "$TERM" = "tmux"* ]] && [[ -n "$TMUX" ]]; then
+  if [ "$1" == "prod" ]; then
+    tmux select-pane -P 'bg=#331C1F';
+  elif [ "$1" == "dev" ]; then
+    tmux select-pane -P 'bg=#192436';
+  elif [ "$1" == "other" ]; then
+    tmux select-pane -P 'bg=#253320';
+  else
+    tmux select-pane -P 'bg=#1b1b1b';
+  fi;
+else
+  if [ "$1" == "prod" ]; then
+    printf '\033]11;#331C1F\007';
+  elif [ "$1" == "dev" ]; then
+    printf '\033]11;#192436\007';
+  elif [ "$1" == "other" ]; then
+    printf '\033]11;#253320\007';
+  else
+    printf '\033]11;#1b1b1b\007';
+  fi
+fi
+
