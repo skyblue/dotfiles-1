2d8522cb333a4665d15a8ac2e0728841d991a7b5
2O4 <35725720+2O4@users.noreply.github.com>
2020-09-09 09:38:29 +0200

    new history alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -7,27 +7,33 @@ alias free="free -m"                                            # Show sizes in 
 alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"  # Manage dotfiles
 alias p="sudo pacman"                                           #
 alias v="$EDITOR"                                               #
+
 alias sv="sudo $EDITOR"                                         #
 alias r="ranger"                                                #
 alias sr="sudo ranger"                                          #
 alias y="youtube-dl -i -x --audio-format best"                  # Download musics
 alias yd="youtube-dl"                                           # Download videos
+
 alias md="mkdir -p"
 alias rd="rmdir"
+
+# cd
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# ls
 alias l="ls --color=auto"                                       # ls
 alias ls="ls --color=auto"                                      # ls
 alias la="ls -A --color=auto"                                   # ls -A
 alias ll="ls -lahF --color=auto --group-directories-first"      # ls -l
 
+# grep
 alias grep="grep --color=auto"                                  #
 alias egrep="egrep --color=auto"                                #
 alias fgrep="fgrep --color=auto"                                #
 
-# Git
+# git
 alias g="git"                                                   # Git
 alias ga="git add"                                              #
 alias gaa="git add --all"                                          #
@@ -38,4 +44,5 @@ alias gpl="git pull"                                            #
 alias gd="git diff"                                             #
 
 alias reload="exec ${SHELL} -l"
+alias h="history"
 
